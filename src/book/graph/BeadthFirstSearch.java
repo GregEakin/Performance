7e01d34bbd104573d53b56169e5312ddea49ddbf
greg <gregory.eakin@gmail.com>
2013-04-26 11:42:47 +0530

    New stuff

---
+++ src/book/graph/BeadthFirstSearch.java
@@ -0,0 +1,11 @@
+package book.graph;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: greg.eakin
+ * Date: 4/26/13
+ * Time: 11:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BeadthFirstSearch {
+}
---
+++ src/book/sets/disjoint/Node.java
@@ -0,0 +1,8 @@
+package book.sets.disjoint;
+
+public class Node {
+    Node p;
+    Node next;
+    int value;
+
+}
