c55d94f950eb8a3cf287557fc2ef4a9576eff840
greg <eakin@cox.net>
2014-11-16 23:19:42 -0800

    More samples

--- src/functional/Compare.java
+++ src/functional/Compare.java
@@ -1,11 +1,14 @@
 package functional;
 
 import org.approvaltests.Approvals;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -51,5 +54,41 @@ public class Compare {
                 .sorted(((Comparator<Person>) Person::ageDifference).reversed())
                 .collect(Collectors.toList());
         Approvals.verifyAll("descendingAge", descendingAge);
+    }
+
+    @Test
+    public void ascendingNameTest() throws Exception {
+        Comparator<Person> compareAscending = (person1, person2) -> person1.getName().compareTo(person2.getName());
+
+        List<Person> ascendingName = people.stream()
+                .sorted(compareAscending)
+                .collect(Collectors.toList());
+        Approvals.verifyAll("ascendingName", ascendingName);
+    }
+
+    @Test
+    public void ascendingNameTest2() throws Exception {
+        final Function<Person, String> byName = person -> person.getName();
+
+        List<Person> ascendingName = people.stream()
+                .sorted(Comparator.comparing(byName))
+                .collect(Collectors.toList());
+        Approvals.verifyAll("ascendingName", ascendingName);
+    }
+
+    @Test
+    public void youngestAgeTest() throws Exception {
+        Optional<Person> youngest = people.stream()
+                .min(Person::ageDifference);
+        Assert.assertTrue(youngest.isPresent());
+        Assert.assertEquals(people.get(0), youngest.get());
+    }
+
+    @Test
+    public void eldestAgeTest() throws Exception {
+        Optional<Person> eldest = people.stream()
+                .max(Person::ageDifference);
+        Assert.assertTrue(eldest.isPresent());
+        Assert.assertEquals(people.get(3), eldest.get());
     }
 }
